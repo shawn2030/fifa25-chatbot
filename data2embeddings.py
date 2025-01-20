d2f2fd62ce664ecfb472aa4a7bab957a9723e064
Shounak Desai <shounakdesai72@gmail.com>
2025-01-20 12:02:50 -0500

    embeddings stored in mongodb

--- data2embeddings.py
+++ data2embeddings.py
@@ -7,25 +7,52 @@ from sentence_transformers import SentenceTransformer
 # Initialize the MongoDB client
 client = MongoClient('localhost', 27017)
 db = client['fifa']
-collection = db['players']
+row_collection = db['row_embeddings']
+attribute_collection = db['attribute_embeddings']
 
 # Load the data from CSV
 def load_data():
-    data = pd.read_csv('data/player_data.csv')
-    data = data.drop('url', axis=1, inplace=True)
-    return data
+    data = pd.read_csv('player_data.csv')
+    data.drop('url', axis=1, inplace=True)
+    data.drop('Unnamed: 0.1', axis=1, inplace=True)
+    data.drop('Unnamed: 0', axis=1, inplace=True)
+
+    row_columns = ['Name', 'Rank', 'Position', 'Team', 'Age', 'Nation', 'League']
+    attribute_columns = ['PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY', 'Acceleration', 'Sprint Speed',
+                         'Positioning', 'Finishing', 'Shot Power', 'Long Shots', 'Volleys', 'Penalties']
+
+    return data, row_columns, attribute_columns
 
 # Save the embeddings to MongoDB
-def process_and_store(dataframe):
+def process_and_store(dataframe, row_columns, attribute_columns):
     model = SentenceTransformer('all-MiniLM-L6-v2') 
     
     for idx, row in dataframe.iterrows():
-        text = row['content']
-        embedding = model.encode(text).tolist()
-        doc = {
-            "id": int(row['id']) if 'id' in row else idx,
-            "title": row.get('title', f"Document {idx}"),
-            "text": text,
-            "embedding": embedding
+
+        # generate row embeddings here
+        row_text = ' '.join([str(row[col]) for col in row_columns if col in row_columns])
+        row_embedding = model.encode(row_text).tolist()
+
+        attribute_text = ' '.join([str(row[col]) for col in attribute_columns if col in attribute_columns])
+        attribute_embedding  = model.encode(attribute_text).tolist()
+
+        row_doc = {
+            "id": idx,
+            "Name" : row['Name'],
+            "Team" : row["Team"],
+            "row_embedding" : row_embedding 
         }
-        collection.insert_one(doc)
+
+        attribute_doc = {
+            "id": idx,
+            "Name": row['Name'],
+            "attribute_embedding": attribute_embedding
+        }
+
+        row_collection.insert_one(row_doc)
+        attribute_collection.insert_one(attribute_doc)
+
+
+if __name__ == '__main__':
+    data, row_columns, attribute_columns = load_data()
+    process_and_store(data, row_columns, attribute_columns)
